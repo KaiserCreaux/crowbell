9fc37ce2eda6994f0bc45f3047d16107868c649c
Todd <KaiserCreaux@users.noreply.github.com>
2025-02-26 15:10:32 -0500

    Small Intro edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: { plugins: [tailwindcss()], },
-  integrations: [preact(), mdx(), sitemap()]
+  integrations: [preact(), mdx(), sitemap()],
+  adapter: cloudflare(),
 })
